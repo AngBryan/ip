ff40f489ff9dacd36f4ff1ab9fe49c5a44dc25ab
AngBryan <AngBryan@protonmail.com>
2026-08-29 10:21:17 +0800

    Add support for echo

--- src/main/java/Bryte.java
+++ src/main/java/Bryte.java
@@ -1,5 +1,8 @@
+import java.util.Scanner;
+
 public class Bryte {
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
         String banner = "██████╗ ██████╗ ██╗   ██╗████████╗███████╗\n"
               + "██╔══██╗██╔══██╗╚██╗ ██╔╝╚══██╔══╝██╔════╝\n"
               + "██████╔╝██████╔╝ ╚████╔╝    ██║   █████╗  \n"
@@ -9,6 +12,18 @@ public class Bryte {
         System.out.println(banner);
         System.out.println("Hello! I'm BRYTE.");
         System.out.println("What can I do for you?");
+        while (true){
+            String command = input.nextLine();
+            if (command.equalsIgnoreCase("exit")){
+                break;
+            }
+
+            System.out.println("____________________________________________________________");
+            System.out.println(command);
+            System.out.println("____________________________________________________________");
+
+        }
+        input.close();
         System.out.println("____________________________________________________________");
         System.out.println("Bye. Hope to see you again soon!");
         System.out.println("____________________________________________________________");
