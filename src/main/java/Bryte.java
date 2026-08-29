a124d731283eb7bae72d94c07ab95bda2fdb4227
AngBryan <AngBryan@protonmail.com>
2026-08-29 10:35:41 +0800

    Add support for list
    Add a switch statement for commands in anticipation for new commands in the future.

--- src/main/java/Bryte.java
+++ src/main/java/Bryte.java
@@ -3,24 +3,41 @@ import java.util.Scanner;
 public class Bryte {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+        String[] list = new String[100];
         String banner = "██████╗ ██████╗ ██╗   ██╗████████╗███████╗\n"
               + "██╔══██╗██╔══██╗╚██╗ ██╔╝╚══██╔══╝██╔════╝\n"
               + "██████╔╝██████╔╝ ╚████╔╝    ██║   █████╗  \n"
               + "██╔══██╗██╔══██╗  ╚██╔╝     ██║   ██╔══╝  \n"
               + "██████╔╝██║  ██║   ██║      ██║   ███████╗\n"
               + "╚═════╝ ╚═╝  ╚═╝   ╚═╝      ╚═╝   ╚══════╝\n";
+        int listCounter = 0;
+        boolean running = true;
+
         System.out.println(banner);
         System.out.println("Hello! I'm BRYTE.");
         System.out.println("What can I do for you?");
-        while (true){
-            String command = input.nextLine();
-            if (command.equalsIgnoreCase("exit")){
-                break;
-            }
 
-            System.out.println("____________________________________________________________");
-            System.out.println(command);
-            System.out.println("____________________________________________________________");
+        while (running){
+            String command = input.nextLine();
+            switch(command.toLowerCase()){
+                case "list":
+                    System.out.println("____________________________________________________________");
+                    for (int i = 0; i < listCounter; i++){
+                        System.out.println(i + ". " + list[i]);
+                    }
+                    System.out.println("____________________________________________________________");
+                    break;
+                case "exit":
+                    running = false;
+                    break;
+                default:
+                    list[listCounter] = command;
+                    listCounter++;
+                    System.out.println("____________________________________________________________");
+                    System.out.println("Added: " + command);
+                    System.out.println("____________________________________________________________");
+
+            }
 
         }
         input.close();
