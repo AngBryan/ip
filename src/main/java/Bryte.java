9a603095018fa3d6f40c4add91349c2823580e50
AngBryan <AngBryan@protonmail.com>
2026-08-29 11:17:00 +0800

    Add mark/unmark command

--- src/main/java/Bryte.java
+++ src/main/java/Bryte.java
@@ -19,7 +19,8 @@ public class Bryte {
 
         while (running){
             String command = input.nextLine();
-            switch(command.toLowerCase()){
+            String[] commandParts = command.split(" ");
+            switch(commandParts[0].toLowerCase()){
                 case "list":
                     System.out.println("____________________________________________________________");
                     System.out.println("Here are the tasks in your list:");
@@ -28,10 +29,23 @@ public class Bryte {
                     }
                     System.out.println("____________________________________________________________");
                     break;
-                    
+
+                case "mark":
+                    System.out.println("____________________________________________________________");
+                    setMark(true, taskList, commandParts);
+                    System.out.println("____________________________________________________________");
+                    break;
+
+                case "unmark":
+                    System.out.println("____________________________________________________________");
+                    setMark(false, taskList, commandParts);
+                    System.out.println("____________________________________________________________");
+                    break;
+
                 case "bye":
                     running = false;
                     break;
+                    
                 default:
                     Task newTask = new Task(command, false);
                     taskList[listCounter] = newTask;
@@ -47,5 +61,34 @@ public class Bryte {
         System.out.println("____________________________________________________________");
         System.out.println("Bye. Hope to see you again soon!");
         System.out.println("____________________________________________________________");
+    }
+
+    //Handles the mark and unmark command 
+    private static void setMark(boolean markStatus, Task[] taskList, String[] commandParts) {
+        if (commandParts.length < 2) {
+            System.out.println("Please specify a task number.");
+            return;
+        }
+        //use of AI for more robust error catching
+        try {
+            int index = Integer.parseInt(commandParts[1]);
+
+            if (index >= 0 && index < taskList.length && taskList[index] != null) {
+                if (markStatus == true){
+                    System.out.println("Nice! I've marked this task as done:");
+                }
+                else{
+                    System.out.println("OK, I've marked this task as not done yet:");
+                }
+                taskList[index].setDone(markStatus);
+                System.out.println("  [" + taskList[index].getStatusIcon() + "] "
+                        + taskList[index].getName());
+            } else {
+                System.out.println("Invalid task number.");
+            }
+        } catch (NumberFormatException e) {
+            System.out.println("Task number must be an integer.");
+        }
+
     }
 }
