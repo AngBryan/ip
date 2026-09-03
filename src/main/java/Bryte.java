20572c110968f9e323fe800ad786404d100bf319
AngBryan <AngBryan@protonmail.com>
2026-09-03 22:17:19 +0800

    Refactor Bryte to use extracted command handlers
    This change improves readability by adhering to the Single Level of Abstraction Principle (SLAP) and avoiding excessively long methods.

--- src/main/java/Bryte.java
+++ src/main/java/Bryte.java
@@ -13,11 +13,11 @@ public class Bryte {
             + "╚═════╝ ╚═╝  ╚═╝   ╚═╝      ╚═╝   ╚══════╝\n";
     private static final String DIVIDER = "____________________________________________________________";
     private static final int MAX_TASKS = 100;
+    private static Task[] taskList = new Task[MAX_TASKS];
+    private static int taskCount = 0;
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        Task[] taskList = new Task[MAX_TASKS];
-        int taskCount = 0;
         boolean isRunning = true;
 
         System.out.println(BANNER);
@@ -31,81 +31,31 @@ public class Bryte {
 
             switch (keyword) {
             case "list":
-                System.out.println(DIVIDER);
-                System.out.println(" Here are the tasks in your list:");
-                for (int i = 0; i < taskCount; i++) {
-                    System.out.println(" " + (i + 1) + "." + taskList[i].toString());
-                }
-                System.out.println(DIVIDER);
+                handleListCommand();
                 break;
 
             case "mark":
                 System.out.println(DIVIDER);
-                setMark(true, taskList, commandParts);
+                setMark(true, commandParts);
                 System.out.println(DIVIDER);
                 break;
 
             case "unmark":
                 System.out.println(DIVIDER);
-                setMark(false, taskList, commandParts);
+                setMark(false, commandParts);
                 System.out.println(DIVIDER);
                 break;
 
             case "todo":
-                if (commandParts.length < 2) {
-                    System.out.println(DIVIDER);
-                    System.out.println(" Please provide a description for the todo.");
-                    System.out.println(DIVIDER);
-                    break;
-                }
-                Todo newTodo = new Todo(commandParts[1]);
-                addTask(newTodo, taskList, taskCount);
-                taskCount++;
+                handleAddTodoCommand(commandParts);
                 break;
 
             case "deadline":
-                if (commandParts.length < 2) {
-                    System.out.println(DIVIDER);
-                    System.out.println(" Please provide a description and deadline.");
-                    System.out.println(DIVIDER);
-                    break;
-                }
-                String[] deadlineParts = commandParts[1].split(" /by ");
-                if (deadlineParts.length < 2) {
-                    System.out.println(DIVIDER);
-                    System.out.println(" Please use /by to specify the deadline.");
-                    System.out.println(DIVIDER);
-                    break;
-                }
-                Deadline newDeadline = new Deadline(deadlineParts[0], deadlineParts[1]);
-                addTask(newDeadline, taskList, taskCount);
-                taskCount++;
+                handleAddDeadlineCommand(commandParts);
                 break;
 
             case "event":
-                if (commandParts.length < 2) {
-                    System.out.println(DIVIDER);
-                    System.out.println(" Please provide a description and duration for the event.");
-                    System.out.println(DIVIDER);
-                    break;
-                }
-                String[] eventParts = commandParts[1].split(" /from ");
-                if (eventParts.length < 2) {
-                    System.out.println(DIVIDER);
-                    System.out.println(" Please use /from and /to to specify the event duration.");
-                    System.out.println(DIVIDER);
-                    break;
-                }
-                String[] timeParts = eventParts[1].split(" /to ");
-                if (timeParts.length < 2) {
-                    System.out.println(DIVIDER);
-                    System.out.println(" Please use /to to specify the end time of the event.");
-                    System.out.println(DIVIDER);
-                    break;
-                }
-                Event newEvent = new Event(eventParts[0], timeParts[0], timeParts[1]);
-                addTask(newEvent, taskList, taskCount);
-                taskCount++;
+                handleAddEventCommand(commandParts);
                 break;
 
             case "bye":
@@ -113,13 +63,7 @@ public class Bryte {
                 break;
 
             default:
-                // Keep creating generic Tasks for backward compatibility or if no keyword matches
-                Task newTask = new Task(command, false);
-                taskList[taskCount] = newTask;
-                taskCount++;
-                System.out.println(DIVIDER);
-                System.out.println(" Added: " + newTask.getName());
-                System.out.println(DIVIDER);
+                handleDefaultCommand(command);
                 break;
             }
         }
@@ -130,14 +74,86 @@ public class Bryte {
         System.out.println(DIVIDER);
     }
 
+    private static void handleListCommand() {
+        System.out.println(DIVIDER);
+        System.out.println(" Here are the tasks in your list:");
+        for (int i = 0; i < taskCount; i++) {
+            System.out.println(" " + (i + 1) + "." + taskList[i].toString());
+        }
+        System.out.println(DIVIDER);
+    }
+
+    private static void handleAddTodoCommand(String[] commandParts) {
+        if (commandParts.length < 2) {
+            System.out.println(DIVIDER);
+            System.out.println(" Please provide a description for the todo.");
+            System.out.println(DIVIDER);
+            return;
+        }
+        Todo newTodo = new Todo(commandParts[1]);
+        addTask(newTodo);
+    }
+
+    private static void handleAddDeadlineCommand(String[] commandParts) {
+        if (commandParts.length < 2) {
+            System.out.println(DIVIDER);
+            System.out.println(" Please provide a description and deadline.");
+            System.out.println(DIVIDER);
+            return;
+        }
+        String[] deadlineParts = commandParts[1].split(" /by ");
+        if (deadlineParts.length < 2) {
+            System.out.println(DIVIDER);
+            System.out.println(" Please use /by to specify the deadline.");
+            System.out.println(DIVIDER);
+            return;
+        }
+        Deadline newDeadline = new Deadline(deadlineParts[0], deadlineParts[1]);
+        addTask(newDeadline);
+    }
+
+    private static void handleAddEventCommand(String[] commandParts) {
+        if (commandParts.length < 2) {
+            System.out.println(DIVIDER);
+            System.out.println(" Please provide a description and duration for the event.");
+            System.out.println(DIVIDER);
+            return;
+        }
+        String[] eventParts = commandParts[1].split(" /from ");
+        if (eventParts.length < 2) {
+            System.out.println(DIVIDER);
+            System.out.println(" Please use /from and /to to specify the event duration.");
+            System.out.println(DIVIDER);
+            return;
+        }
+        String[] timeParts = eventParts[1].split(" /to ");
+        if (timeParts.length < 2) {
+            System.out.println(DIVIDER);
+            System.out.println(" Please use /to to specify the end time of the event.");
+            System.out.println(DIVIDER);
+            return;
+        }
+        Event newEvent = new Event(eventParts[0], timeParts[0], timeParts[1]);
+        addTask(newEvent);
+    }
+
+    private static void handleDefaultCommand(String command) {
+        // Keep creating generic Tasks for backward compatibility or if no keyword matches
+        Task newTask = new Task(command, false);
+        taskList[taskCount] = newTask;
+        taskCount++;
+        System.out.println(DIVIDER);
+        System.out.println(" Added: " + newTask.getName());
+        System.out.println(DIVIDER);
+    }
+
     /**
      * Handles the execution of mark and unmark commands on the task list.
      *
      * @param markStatus The status to set: {@code true} for marked as done, {@code false} for undone.
-     * @param taskList The array containing the user's tasks.
      * @param commandParts The array of string tokens from the user input.
      */
-    private static void setMark(boolean markStatus, Task[] taskList, String[] commandParts) {
+    private static void setMark(boolean markStatus, String[] commandParts) {
         if (commandParts.length < 2) {
             System.out.println(" Please specify a task number.");
             return;
@@ -146,7 +162,7 @@ public class Bryte {
         try {
             int index = Integer.parseInt(commandParts[1]) - 1;
 
-            if (index >= 0 && index < taskList.length && taskList[index] != null) {
+            if (index >= 0 && index < taskCount && taskList[index] != null) {
                 if (markStatus) {
                     System.out.println(" Nice! I've marked this task as done:");
                 } else {
@@ -166,15 +182,20 @@ public class Bryte {
      * Adds a task to the task list and prints the confirmation message.
      *
      * @param task The task to be added.
-     * @param taskList The array containing the user's tasks.
-     * @param taskCount The current number of tasks.
      */
-    private static void addTask(Task task, Task[] taskList, int taskCount) {
+    private static void addTask(Task task) {
+        if (taskCount >= MAX_TASKS) {
+            System.out.println(DIVIDER);
+            System.out.println(" Task list is full. Cannot add more tasks.");
+            System.out.println(DIVIDER);
+            return;
+        }
         taskList[taskCount] = task;
+        taskCount++;
         System.out.println(DIVIDER);
         System.out.println(" Got it. I've added this task:");
         System.out.println("   " + task.toString());
-        System.out.println(" Now you have " + (taskCount + 1) + " tasks in the list.");
+        System.out.println(" Now you have " + taskCount + " tasks in the list.");
         System.out.println(DIVIDER);
     }
 }
